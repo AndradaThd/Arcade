fa0ab2d1102b36fdd4af94b65a2c0dcffe37f3e9
AndradaThd <75580814+AndradaThd@users.noreply.github.com>
2022-08-27 14:10:01 +0300

    Update MovingPlatform.h

--- MovingPlatform.h
+++ MovingPlatform.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -12,15 +11,15 @@ class OBSTACLEASSAULT_API AMovingPlatform : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
+	
 	AMovingPlatform();
 
 protected:
-	// Called when the game starts or when spawned
+
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
+
 	virtual void Tick(float DeltaTime) override;
 
 private:
